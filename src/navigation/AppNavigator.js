dae8a72825ff2b1d5bd153b6ae2b3447f5d08990
João Marcos Tavares <joao_pe10@hotmail.com>
2025-11-22 18:42:30 -0300

    AppNavigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+
+// Telas Principais
 import DashboardScreen from '../screens/Main/DashboardScreen';
+
+// Telas de Configuração
+import SettingsMenuScreen from '../screens/Main/Settings/SettingsMenuScreen';
+import AccessibilityScreen from '../screens/Main/Settings/AccessibilityScreen';
 
 const Stack = createStackNavigator();
 
@@ -10,9 +16,27 @@ export default function AppNavigator() {
       <Stack.Screen 
         name="Dashboard" 
         component={DashboardScreen} 
-        options={{ title: 'SIGO Mobile' }} 
+        options={{ headerShown: false }} 
       />
-      {/* Aqui entrarão as telas de Ocorrências, Perfil, etc. */}
+      
+      {/* Rotas de Configuração */}
+      <Stack.Screen 
+        name="SettingsMenu" 
+        component={SettingsMenuScreen} 
+        options={{ title: 'Configurações' }} 
+      />
+      <Stack.Screen 
+        name="Accessibility" 
+        component={AccessibilityScreen} 
+        options={{ title: 'Acessibilidade' }} 
+      />
+      
+      {/* Placeholders para não dar erro se clicar */}
+      <Stack.Screen name="IncidentRegistration" component={DashboardScreen} options={{title: 'Em breve'}}/>
+      <Stack.Screen name="MyIncidents" component={DashboardScreen} options={{title: 'Em breve'}}/>
+      <Stack.Screen name="EditProfile" component={DashboardScreen} options={{title: 'Em breve'}}/>
+      <Stack.Screen name="Notifications" component={DashboardScreen} options={{title: 'Em breve'}}/>
+      <Stack.Screen name="ChangePassword" component={DashboardScreen} options={{title: 'Em breve'}}/>
     </Stack.Navigator>
   );
 }
